80e3f5f5bc6df6deb9cb87c77b0506b7f3e64cdc
Xiangrui Dong <samdongxiangrui@outlook.com>
2022-04-26 12:38:14 +0800

    integrate executor, todoList and repository

--- src/main/java/todo/Main.java
+++ src/main/java/todo/Main.java
@@ -1,17 +1,23 @@
 package todo;
 
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Scanner;
+import todo.repository.TodoListMemoryRepository;
+import todo.repository.TodoListRepository;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Welcome");
         Scanner scanner = new Scanner(System.in);
-//        TodoList todoList = new TodoList(new LinkedHashMap<>());
-//        Executor executor = new Executor(todoList);
-//        while (scanner.hasNext()) {
-//            String command = scanner.nextLine();
-//            executor.execute(command);
-//        }
+        TodoListRepository todoListRepository = new TodoListMemoryRepository(new LinkedHashMap<>());
+        TodoList todoList = new TodoList(todoListRepository);
+        Executor executor = new Executor(todoList);
+        while (scanner.hasNext()) {
+            String command = scanner.nextLine();
+            List<String> results = executor.execute(command);
+            results.forEach(System.out::println);
+        }
     }
 }
